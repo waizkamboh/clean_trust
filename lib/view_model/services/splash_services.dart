1e5a0b42de3bb06cd1a072726a53134d53895337
raomudassar34 <112935849+raomudassar34@users.noreply.github.com>
2025-12-30 23:57:07 +0500

    Add splash services

--- lib/view_model/services/splash_services.dart
+++ lib/view_model/services/splash_services.dart
@@ -1,22 +1,22 @@
-// import 'dart:async';
-// import 'package:get/get.dart';
-// import '../../helper/routes/routes_name.dart';
-// import '../user_preference/user_preference.dart';
-//
-// class SplashServices {
-//   final UserPreference userPreference = UserPreference();
-//
-//   void isLogin() async {
-//     String? token = await userPreference.getToken();
-//
-//     print('Saved Token: $token');
-//
-//     Timer(const Duration(seconds: 3), () {
-//       if (token == null || token.isEmpty) {
-//         Get.toNamed(RouteName.loginScreen);
-//       } else {
-//         Get.toNamed(RouteName.bottomNavScreen);
-//       }
-//     });
-//   }
-// }
+import 'dart:async';
+import 'package:get/get.dart';
+import '../../helper/routes/routes_name.dart';
+import '../user_preference/user_preference.dart';
+
+class SplashServices {
+  final UserPreference userPreference = UserPreference();
+
+  void isLogin() async {
+    String? token = await userPreference.getToken();
+
+    print('Saved Token: $token');
+
+    Timer(const Duration(seconds: 3), () {
+      if (token == null || token.isEmpty) {
+        Get.toNamed(RouteName.loginScreen);
+      } else {
+        Get.toNamed(RouteName.bottomNavScreen);
+      }
+    });
+  }
+}
